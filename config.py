d836f56d949aa4b03094dd03eb02cee8d79914f2
adityaaboss <adityarajyadav04@gmail.com>
2025-06-19 15:36:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+BOT_TOKEN = "7773196210:AAFdbW5dHCZQ9sO6b79Zm-sWFqTMjOiQqVc"
+
+API_ID = 20285525
+API_HASH = "9fcf99b60232fcca9950df54106e435f"
+
+DB_CHANNEL = -1002854475119
+BOT_OWNER = 5494945309
+
+SHORTNER_API = "5b3d571d469b938fb2f44299604f6f536e3c4bcf"
+SHORTNER_URL = "https://shortxlinks.com/api"
+
+UPDATES_CHANNELS = [
+    "https://t.me/modswalebhaiya",
+    "https://t.me/Piratessoftelegram"
+]
+
+LANG = "en"
